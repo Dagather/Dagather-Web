f430bd95f6e7d07458789a95768f89c11694709d
Kwon <khj9709@icloud.com>
2021-02-11 16:49:34 +0900

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { withRouter } from 'react-router-dom';
+
+import Router from 'Router';
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default withRouter(App);
