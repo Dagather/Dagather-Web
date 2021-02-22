c601c601b1717ac85313203b4d20e93e8d218e7a
Kwon <khj9709@icloud.com>
2021-02-23 01:17:46 +0900

    Add Introduce-text Constants #3

---
+++ src/Constants/introduce-text.js
@@ -0,0 +1,5 @@
+export const serviceIntro = ['Dagather는 RPA Tool인 UiPath를 이용해 생성된 로봇 프로세스를 다루는 서비스입니다.',
+  '로봇 프로세스의 스크립트를 업로드 및 다운로드 할 수 있습니다.',
+  '또한 Dagather Community를 통해 사용자가 필요로 하는 로봇 프로세스를 요청 할 수 있습니다.'];
+
+export default serviceIntro;
