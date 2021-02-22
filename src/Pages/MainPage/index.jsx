cbb44025030b006b49004386a01f22ae0bdba340
Kwon <khj9709@icloud.com>
2021-02-23 01:18:31 +0900

    Make responsive using media query #3

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import NavBar from 'Components/NavBar';
 import Jumbotron from 'Components/Jumbotron';
+import MainTab from 'Components/MainTab';
+import Tab from 'Components/MainTab/Tab';
+
+import introduceText from 'Constants/introduce-text';
 
 function Mainpage() {
+  const getTab = () => (
+    introduceText.map((text, index) => (
+      <Tab key={text} title={(index + 1).toString()}>
+        {text}
+      </Tab>
+    ))
+  );
+
   return (
     <>
       <NavBar />
       <Jumbotron />
       <div className="container">
-        <div className="mainPage" />
+        <div className="mainPage">
+          <MainTab title="서비스 소개">
+            {getTab()}
+          </MainTab>
+        </div>
       </div>
     </>
   );
