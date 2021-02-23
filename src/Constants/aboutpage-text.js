5eff8c63163da02bde423d9365c0b5178c91be9f
jegyun-Park <parkjekyun@gmail.com>
2021-02-23 19:47:36 +0900

    about page

---
+++ src/Constants/aboutpage-text.js
@@ -0,0 +1,10 @@
+export const aboutPageText = [
+  [
+    '저희 Dagather는 Uipath 사용자를 중심으로, Script를 공유하고 향상시킴으로써 더 나은 업무 환경을 제공하고, 업무 효율성을 높이기 위해 이번 프로젝트를 기획하였습니다.',
+    '프로젝트 개요',
+  ],
+  ['', '구성원'],
+  ['', '기술 스택'],
+];
+
+export default aboutPageText;
