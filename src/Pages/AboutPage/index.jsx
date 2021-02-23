e416911e366aa51b45688047c14a0e526f578c4a
jegyun-Park <parkjekyun@gmail.com>
2021-02-23 19:48:12 +0900

    about page , demo

--- src/Pages/AboutPage/index.jsx
+++ src/Pages/AboutPage/index.jsx
@@ -1,14 +1,32 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-unused-vars */
+
 import React from 'react';
 
 import NavBar from 'Components/NavBar';
+import MainTab from 'Components/MainTab';
+import Tab from 'Components/MainTab/Tab';
+
+import uipathlogo from 'Assets/img/logo/uipath-logo.jpeg';
+import aboutpageText from 'Constants/aboutpage-text';
 
 function AboutPage() {
+  const getText = () =>
+    aboutpageText.map((text) => <Tab key={text[0]}>{text[0]}</Tab>);
+
   return (
     <>
       <NavBar />
-      <div className="aboutPage">
-        Hello This is AboutPage
+      <div className="container">
+        <div className="aboutPage">
+          <MainTab title="프로젝트 개요" />
+          <img src={uipathlogo} alt={uipathlogo} />
+          {getText()}
+          <MainTab title="구성원" />
+          <MainTab title="기술 스택" />
+        </div>
       </div>
+      <footer>&copy; 2021, Dagather</footer>
     </>
   );
 }
