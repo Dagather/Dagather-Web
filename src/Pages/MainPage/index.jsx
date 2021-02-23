9650fbae809ccab7409f36c6648774fbfd63db19
Kwon <khj9709@icloud.com>
2021-02-23 18:20:58 +0900

    Apply RPA란? mainTab #3

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -5,12 +5,28 @@ import Jumbotron from 'Components/Jumbotron';
 import MainTab from 'Components/MainTab';
 import Tab from 'Components/MainTab/Tab';
 
-import introduceText from 'Constants/introduce-text';
+import { serviceIntro, rpaIntro } from 'Constants/introduce-text';
+import happy from 'Assets/img/icon/happy.svg';
+import project from 'Assets/img/icon/project.svg';
+import sandClock from 'Assets/img/icon/sandClock.svg';
+import typo from 'Assets/img/icon/typo.svg';
 
 function Mainpage() {
-  const getTab = () => (
-    introduceText.map((text, index) => (
+  const getImgTag = (imgsrc, alt) => (
+    <img src={imgsrc} alt={alt} />
+  );
+  const imgArray = [getImgTag(sandClock, 'sandClock'), getImgTag(happy, 'happy'), getImgTag(typo, 'typo'), getImgTag(project, 'project')];
+  const getServiceTab = () => (
+    serviceIntro.map((text, index) => (
       <Tab key={text[1]} title={(index + 1).toString()} subTitle={text[1]}>
+        {text[0]}
+      </Tab>
+    ))
+  );
+
+  const getRPATab = () => (
+    rpaIntro.map((text, index) => (
+      <Tab key={text[1]} title={imgArray[index]} subTitle={text[1]}>
         {text[0]}
       </Tab>
     ))
@@ -23,10 +39,10 @@ function Mainpage() {
       <div className="container">
         <div className="mainPage">
           <MainTab title="서비스 소개">
-            {getTab()}
+            {getServiceTab()}
           </MainTab>
           <MainTab title="RPA란?">
-            testing
+            {getRPATab()}
           </MainTab>
         </div>
       </div>
