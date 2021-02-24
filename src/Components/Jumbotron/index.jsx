a6b6c8daeb17c7689c6aa5ad1a9fb3a3311403d6
Kwon <khj9709@icloud.com>
2021-02-24 16:53:16 +0900

    Add second Jumbotron #3

--- src/Components/Jumbotron/index.jsx
+++ src/Components/Jumbotron/index.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 
-import uiPathConfig from 'Config/uipathConfig';
+import PropTypes from 'prop-types';
 
 import Button from 'Components/Button';
 
-import background from 'Assets/img/background/workingPeople.jpg';
-
-function Jumbotron() {
+function Jumbotron(props) {
+  const { backgroundSrc, title, content, onClick, buttonName } = props;
   return (
     <div className="jumbotron">
       <div className="jumbotron__img">
-        <img src={background} alt="jumboImg" />
+        <img src={backgroundSrc} alt="jumboImg" />
       </div>
       <div className="jumbotron__text">
         <div className="jumbotron__text__title">
-          Create Your Own Robot.
+          {title}
         </div>
         <div className="jumbotron__text__desc">
-          Just Order and Drink Coffee.
+          {content}
         </div>
+        {onClick
+        && (
         <div className="jumbotron__text__btn">
-          <Button onClick={uiPathConfig}>Connect UiPath &nbsp;&nbsp; &gt;</Button>
-        </div>
+          <Button onClick={onClick}>{buttonName}</Button>
+        </div>)}
       </div>
     </div>
   );
 }
+
+Jumbotron.propTypes = {
+  backgroundSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  buttonName: PropTypes.string,
+};
+
+Jumbotron.defaultProps = {
+  onClick: null,
+  buttonName: '',
+};
 
 export default Jumbotron;
--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+
 import NavBar from 'Components/NavBar';
 import Jumbotron from 'Components/Jumbotron';
 import MainTab from 'Components/MainTab';
 import Tab from 'Components/MainTab/Tab';
 
+import uiPathConfig from 'Config/uipathConfig';
+
 import { serviceIntro, rpaIntro } from 'Constants/introduce-text';
+import { firstJumboTextInMain, secondJumboTextInMain } from 'Constants/jumbotron-text';
+
 import happy from 'Assets/img/icon/happy.svg';
 import project from 'Assets/img/icon/project.svg';
 import sandClock from 'Assets/img/icon/sandClock.svg';
 import typo from 'Assets/img/icon/typo.svg';
+import firstBackground from 'Assets/img/background/workingPeople.jpg';
+import secondBackground from 'Assets/img/background/sittingPeople.png';
 
 function Mainpage() {
+  const history = useHistory();
   const getImgTag = (imgsrc, alt) => (
     <img src={imgsrc} alt={alt} />
   );
@@ -35,7 +44,13 @@ function Mainpage() {
   return (
     <>
       <NavBar />
-      <Jumbotron />
+      <Jumbotron
+        backgroundSrc={firstBackground}
+        onClick={uiPathConfig}
+        title={firstJumboTextInMain.title}
+        content={firstJumboTextInMain.content}
+        buttonName="Connect UiPath &nbsp;&nbsp; &gt;"
+      />
       <div className="container">
         <div className="mainPage">
           <MainTab title="서비스 소개">
@@ -46,6 +61,13 @@ function Mainpage() {
           </MainTab>
         </div>
       </div>
+      <Jumbotron
+        backgroundSrc={secondBackground}
+        title={secondJumboTextInMain.title}
+        content={secondJumboTextInMain.content}
+        onClick={() => history.push({ pathname: '/robot' })}
+        buttonName="Getting Started"
+      />
     </>
   );
 }
