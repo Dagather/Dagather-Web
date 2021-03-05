c4f1a07b041935f9853285131abceeda069ba5aa
Kwon <khj9709@icloud.com>
2021-03-05 23:44:44 +0900

    Create Jumbotron to CommunityPage #9

--- src/Pages/CommunityPage/index.jsx
+++ src/Pages/CommunityPage/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import NavBar from 'Components/NavBar';
+import Jumbotron from 'Components/Jumbotron';
+import Footer from 'Components/Footer';
+
+import communityBg from 'Assets/img/background/community.jpg';
 
 function CommunityPage() {
   return (
     <>
       <NavBar />
-      <div className="communityPage">
-        Hello This is CommunityPage
-      </div>
+      <Jumbotron title="Feel free to talk." content="Always available for everyone" backgroundSrc={communityBg} />
+
+      <Footer />
     </>
   );
 }
