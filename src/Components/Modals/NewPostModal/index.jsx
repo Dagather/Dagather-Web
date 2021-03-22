86c83b0e0d45fab8053e4a5e3f2cbcb13dd68e87
Kwon <khj9709@icloud.com>
2021-03-22 22:50:57 +0900

    Add temporal user/pw input #11

--- src/Components/Modals/NewPostModal/index.jsx
+++ src/Components/Modals/NewPostModal/index.jsx
@@ -20,18 +20,31 @@ function NewPostModal(props) {
   const [title, setTitle] = useState('');
   const titleHandler = (e) => setTitle(e.target.value);
 
+  const [author, setAuthor] = useState('');
+  const authorHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const [password, setPassword] = useState('');
+  const pwHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
   const [file, setFile] = useState(null);
   const fileHandler = (e) => {
     setFile(e.target.files[0]);
     console.log(file);
   };
 
+  const [showMsg, setShowMsg] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const options = (
     <>
       <option disabled>분류</option>
-      <option selected value="업로드">업로드</option>
+      <option selected value="업로드">
+        업로드
+      </option>
       <option value="수정">수정</option>
       <option value="기타">기타</option>
     </>
@@ -40,10 +53,10 @@ function NewPostModal(props) {
   const parseDate = () => {
     const date = new Date();
     const year = date.getFullYear();
-    const month = (`0${1 + date.getMonth()}`).slice(-2);
-    const day = (`0${date.getDate()}`).slice(-2);
-    const hour = (`0${date.getHours()}`).slice(-2);
-    const minute = (`0${date.getMinutes()}`).slice(-2);
+    const month = `0${1 + date.getMonth()}`.slice(-2);
+    const day = `0${date.getDate()}`.slice(-2);
+    const hour = `0${date.getHours()}`.slice(-2);
+    const minute = `0${date.getMinutes()}`.slice(-2);
 
     return `${year}-${month}-${day} ${hour}:${minute}`;
   };
@@ -55,7 +68,8 @@ function NewPostModal(props) {
     await newPostRef.set({
       category: optionMapper[selectedOption],
       title,
-      author: 'Hyukjin',
+      author,
+      password,
       created_at: parseDate(),
       content,
     });
@@ -63,30 +77,79 @@ function NewPostModal(props) {
     toggle();
   };
 
+  const isInputValid = () => {
+    if (author && password && title && content) {
+      setShowMsg(false);
+      sendQuery();
+    } else {
+      setShowMsg(true);
+    }
+  };
+
   return (
     <>
       <div className="newPostModal">
         <div className="newPostModal__header">
-          <div className="newPostModal__header__main">
-            새 글 작성
-          </div>
+          <div className="newPostModal__header__main">새 글 작성</div>
           <div className="newPostModal__header__input">
             <Label for="optSelect" />
-            <Input className="newPostModal__header__input__opt" id="optSelect" type="select" value={selectedOption} onChange={optionHandler}>
+            <Input
+              className="newPostModal__header__input__opt"
+              id="optSelect"
+              type="select"
+              value={selectedOption}
+              onChange={optionHandler}
+            >
               {options}
             </Input>
-            <Input className="newPostModal__header__input__title" value={title} onChange={titleHandler} placeholder="제목을 입력하세요" type="textarea" />
+            <Input
+              className="newPostModal__header__input__title"
+              value={title}
+              onChange={titleHandler}
+              placeholder="제목을 입력하세요"
+              type="textarea"
+            />
           </div>
         </div>
         <ReactQuill theme="snow" value={content} onChange={setContent} />
         <div className="newPostModal__file">
-          <Input type="file" onChange={fileHandler}>파일 업로드</Input>
+          <Input type="file" onChange={fileHandler}>
+            파일 업로드
+          </Input>
         </div>
+        <div className="newPostModal__login">
+          <Input
+            className="newPostModal__login__author"
+            value={author}
+            onChange={authorHandler}
+            placeholder="작성자"
+            type="text"
+          >
+            작성자
+          </Input>
+          <Input
+            className="newPostModal__login__password"
+            value={password}
+            onChange={pwHandler}
+            placeholder="비밀번호"
+            type="password"
+          >
+            비밀번호
+          </Input>
+        </div>
+        {showMsg && <p>제목,내용,작성자 및 패스워드를 모두 입력하세요</p>}
         <div className="newPostModal__footer">
-          <Button onClick={sendQuery} className="newPostModal__footer__save"> 완료 </Button>
+          <Button
+            onClick={isInputValid}
+            className="newPostModal__footer__save"
+          >
+            완료
+          </Button>
           <Button onClick={toggle}> 닫기 </Button>
         </div>
-        {isLoading && <Spinner className="newPostModal__loader" color="success" />}
+        {isLoading && (
+          <Spinner className="newPostModal__loader" color="success" />
+        )}
       </div>
     </>
   );
