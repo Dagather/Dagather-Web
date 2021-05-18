a0b74a5febe2137a43b88f365413d5503b37b100
jegyun-Park <parkjekyun@gmail.com>
2021-05-18 21:06:23 +0900

    About page demo

---
+++ src/Constants/aboutpage-members.js
@@ -0,0 +1,7 @@
+export const aboutPageMember = [
+  ['권혁진', '정보', '한양대학교 ERICA 캠퍼스 소프트웨어학부 컴퓨터전공'],
+  ['박재용', '정보', '한양대학교 ERICA 캠퍼스 소프트웨어학부 컴퓨터전공'],
+  ['박제균', '정보', '한양대학교 ERICA 캠퍼스 소프트웨어학부 컴퓨터전공'],
+];
+
+export default aboutPageMember;
--- src/Pages/AboutPage/index.jsx
+++ src/Pages/AboutPage/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 import NavBar from 'Components/NavBar';
@@ -6,17 +8,36 @@ import Tab from 'Components/MainTab/Tab';
 
 import uipathlogo from 'Assets/img/logo/uipath-logo.jpeg';
 import aboutpageText from 'Constants/aboutpage-text';
+import aboutpageMember from 'Constants/aboutpage-members';
+
+import reactLogo from 'Assets/img/logo/react-logo.jpeg';
+import firebaseLogo from 'Assets/img/logo/firebase-logo.jpeg';
+import projectReport from 'Assets/pdf/capstonereport.pdf';
+
 
 function AboutPage() {
   const uiPathImage = <img src={uipathlogo} alt={uipathlogo} />;
   const getText = () => (
     aboutpageText.map((text, index) => (
-      <Tab title={!index && uiPathImage} key={text[0]}>
+      <Tab title={!index && uiPathImage} key={text[2]}>
         <>
           {text[0]}
         </>
       </Tab>
     )));
+
+  const getMember = () => (
+    aboutpageMember.map((text, index) => (
+      <Tab title={text[0]} key={text[1]}>
+        <>
+          <li>{text[1]}</li>
+          <li>{text[2]}</li>
+        </>
+      </Tab>
+    )));
+
+
+
 
   return (
     <>
@@ -26,8 +47,13 @@ function AboutPage() {
           <MainTab title="프로젝트 개요">
             {getText()}
           </MainTab>
-          <MainTab title="구성원" />
-          <MainTab title="기술 스택" />
+          <MainTab title="구성원">
+            {getMember()}
+          </MainTab>
+          <MainTab title="기술 스택">
+            <img src={reactLogo} alt="react-Logo" />
+            <img src={firebaseLogo} alt="react-Logo" />
+          </MainTab>
         </div>
       </div>
       <footer>&copy; 2021, Dagather</footer>
