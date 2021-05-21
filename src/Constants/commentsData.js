009eb225d1d7babbe0e1c6b9c1ae7d508a0c3b14
jegyun-Park <parkjekyun@gmail.com>
2021-05-21 18:30:47 +0900

    Create Comment component

---
+++ src/Constants/commentsData.js
@@ -0,0 +1,11 @@
+const commentsData = [
+  {
+    authorUrl: '#',
+    avatarUrl: '#avatarUrl',
+    createdAt: new Date(),
+    fullName: 'Jegyun',
+    text: '댓글 test ',
+  },
+];
+
+export default commentsData;
